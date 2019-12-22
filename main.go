acbf84d7588fe6bd7ae6a69de5a76b50d4912edf
Luis <Lucifer153790@gmail.com>
2019-12-22 13:40:41 +0800

    add: add open database connection add: add get raw database connection add: add close database connection

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package sorm
 
+import (
+	"database/sql"
+)
+
 type model interface {
-	Instant(obj interface{})
+	instant(obj interface{})
 }
 
 type Model struct {
@@ -9,6 +13,36 @@ type Model struct {
 }
 
 func Make(mod model) (object interface{}) {
-	mod.Instant(mod)
+	mod.instant(mod)
 	return mod
+}
+
+type DB struct {
+	db *sql.DB
+}
+
+func Open(driver, source string) (db *DB, err error) {
+	if len(driver) == 0 || len(source) == 0 {
+		return nil, InvalidDatabaseSource
+	}
+
+	var sqlDB *sql.DB
+	if sqlDB, err = sql.Open(driver, source); err != nil {
+		return nil, err
+	} else if err = sqlDB.Ping(); err != nil {
+		return nil, err
+	}
+
+	db = &DB{
+		db: sqlDB,
+	}
+	return db, nil
+}
+
+func (d *DB) GetRawDB() (db *sql.DB) {
+	return d.db
+}
+
+func (d *DB) Close() (err error) {
+	return d.db.Close()
 }
--- main_test.go
+++ main_test.go
@@ -1,9 +1,28 @@
 package sorm
 
 import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+	_ "github.com/mattn/go-sqlite3"
+	"os"
+	"path/filepath"
 	"reflect"
+	"sync"
 	"testing"
 )
+
+var TestData sync.Map
+
+const TestConnectKey = "db"
+
+func init() {
+	if db, err := OpenTestConnection(); err != nil {
+		panic(fmt.Sprintf("No error should happen when connecting to test database, but got err=%+v", err))
+	} else {
+		TestData.Store(TestConnectKey, db)
+	}
+}
 
 func TestMake(t *testing.T) {
 	type Table struct {
@@ -44,6 +63,117 @@ func TestMake(t *testing.T) {
 				gotObject.(*Table).Field3 != st1.Field3 ||
 				gotObject.(*Table).Model.Object == nil {
 				t.Errorf("Make() = %v, want %v", gotObject, tt.wantObject)
+			}
+		})
+	}
+}
+
+func TestOpen(t *testing.T) {
+	type args struct {
+		driver string
+		source string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantDb  bool
+		wantErr bool
+	}{
+		{
+			name: "normal: sqlite3",
+			args: args{
+				driver: "sqlite3",
+				source: filepath.Join(os.TempDir(), "sorm.db"),
+			},
+			wantDb:  true,
+			wantErr: false,
+		},
+		{
+			name: "exception: nil driver",
+			args: args{
+				source: filepath.Join(os.TempDir(), "sorm.db"),
+			},
+			wantDb:  false,
+			wantErr: true,
+		},
+		{
+			name: "exception: error source",
+			args: args{
+				driver: "postgres",
+				source: "1234",
+			},
+			wantDb:  false,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotDb, err := Open(tt.args.driver, tt.args.source)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Open() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if (gotDb != nil) != tt.wantDb {
+				t.Errorf("Open() gotDb = %v, want %v", gotDb, tt.wantDb)
+			}
+		})
+	}
+}
+
+func OpenTestConnection() (db *DB, err error) {
+	return Open("sqlite3", filepath.Join(os.TempDir(), "sorm.db"))
+}
+
+func TestDB_GetRawDB(t *testing.T) {
+	var d *DB
+	if i, ok := TestData.Load(TestConnectKey); !ok || i == nil {
+		panic("failed to load database connection")
+	} else {
+		d = i.(*DB)
+	}
+
+	tests := []struct {
+		name   string
+		fields *DB
+		wantDb *sql.DB
+	}{
+		{
+			name:   "normal",
+			fields: d,
+			wantDb: d.db,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotDb := tt.fields.GetRawDB(); !reflect.DeepEqual(gotDb, tt.wantDb) {
+				t.Errorf("GetRawDB() = %v, want %v", gotDb, tt.wantDb)
+			}
+		})
+	}
+}
+
+func TestDB_Close(t *testing.T) {
+	var d *DB
+	if i, ok := TestData.Load(TestConnectKey); !ok || i == nil {
+		panic("failed to load database connection")
+	} else {
+		d = i.(*DB)
+	}
+	tests := []struct {
+		name    string
+		fields  *DB
+		wantErr bool
+	}{
+		{
+			name:    "normal",
+			fields:  d,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := tt.fields.Close(); (err != nil) != tt.wantErr {
+				t.Errorf("Close() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
 	}
