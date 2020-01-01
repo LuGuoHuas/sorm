2cc3963ae4fe562b4ccdf990d9e84be626ab0858
Luis <lu_guohua@163.com>
2020-01-01 18:31:59 +0800

    add: init logger

---
+++ logger.go
@@ -0,0 +1,17 @@
+package sorm
+
+type logger interface {
+	Print(v ...interface{})
+}
+
+type LogWriter interface {
+	Println(v ...interface{})
+}
+
+type Logger struct {
+	LogWriter
+}
+
+func (logger Logger) Print(values ...interface{}) {
+	logger.Println(values...)
+}
